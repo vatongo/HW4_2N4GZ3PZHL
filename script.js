86dbe93c62bdfd63bda1a4190f8f361bcc5852f3
vatongo <vatongo21@gmail.com>
2020-09-12 21:31:14 +0000

    first initial

---
+++ script.js
@@ -0,0 +1,48 @@
+const getInfo = () => {
+  const response = fetch("https://gtl-covid-api.herokuapp.com/api")
+    .then((response) => response.json())
+
+    .then((data) => {
+      let totalConfirm = 0;
+      let totalDeaths = 0;
+      data["Covid 19 Cases"].forEach((last, index) => {
+        totalDeaths += Number(last.Deaths);
+        totalConfirm += Number(last.Confirmed);
+        if (index + 1 === data["Covid 19 Cases"].length) {
+          console.log(totalDeaths);
+          document.getElementById(
+            "second"
+          ).innerHTML = `<div><p>Total Cases</p><span>${totalDeaths}</span></div>`;
+        }
+
+        document.querySelector("#covid-table").insertAdjacentHTML(
+          "afterbegin",
+
+          "<tr>" +
+            "<td>" +
+            last.Country +
+            "</td>" +
+            "<td>" +
+            last.Active +
+            "</td>" +
+            "<td>" +
+            last.Recoveries +
+            "</td>" +
+            "<td>" +
+            last.Deaths +
+            "</td>" +
+            "<td>" +
+            last.Confirmed +
+            "</td>" +
+            "</tr>"
+        );
+      });
+
+      data["Covid 19 Cases"].forEach((item, index) => {
+        console.log(item.Deaths);
+      });
+    })
+
+    .catch((error) => console.log(error));
+};
+getInfo();
